f8c40ac39490daceae2dfea26e589cc87a35c6c6
Roman Klos <roman.klos@coex.cz>
2014-06-08 15:30:55 +0200

    Generic skin for windows phone (PhoneJS doesn't work on WP8.1)

--- www/index.js
+++ www/index.js
@@ -1,7 +1,9 @@
 ﻿window.Pyly = window.Pyly || { };
 
 // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
-// DevExpress.devices.current({ platform: "generic" });
+if(DevExpress.devices.current().platform === 'win8') {
+    DevExpress.devices.current({ platform: "generic" });
+}
 
 $(function() {
     Pyly.app = new DevExpress.framework.html.HtmlApplication({
